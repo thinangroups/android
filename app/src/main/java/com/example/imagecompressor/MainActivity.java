4ad2eee6f2e7cdf1072e2e7778f529b2e58b8639
jebulin <jebulin3@gmail.com>
2022-09-14 00:40:07 +0530

    first commit

---
+++ app/src/main/java/com/example/imagecompressor/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.imagecompressor;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.Normalizer;
+import java.util.List;
+
+import id.zelory.compressor.Compressor;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgOriginal, imgCompressed;
+    TextView txtOriginal, txtCompressed, txtQuality;
+    EditText txtHeight, txtWidth;
+    SeekBar seekBar;
+    Button btnPick, btnCompress;
+    private File originalImage, compressedImage;
+    private static String filepath;
+    public static final int RESULT_IMAGE = 1;
+    File path = new File(System.getenv("External_Storage") + "/mycompressor");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        askPermission();
+        imgOriginal = findViewById(R.id.imgOriginal);
+        imgCompressed = findViewById(R.id.imgCompressed);
+        txtOriginal = findViewById(R.id.txtOriginal);
+        txtCompressed = findViewById(R.id.txtCompressed);
+        txtQuality = findViewById(R.id.txtQuality);
+        txtHeight = findViewById(R.id.txtHeight);
+        txtWidth = findViewById(R.id.txtWidth);
+        seekBar = findViewById(R.id.seekBar);
+        btnPick = findViewById(R.id.btnPick);
+        btnCompress = findViewById(R.id.btnCompress);
+
+        filepath = path.getAbsolutePath();
+        if (!path.exists()) {
+            path.mkdir();
+        }
+        btnPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtQuality.setText("Quality " + i);
+                seekBar.setMax(100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        btnCompress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int quality = seekBar.getProgress();
+                int width = Integer.parseInt(txtWidth.getText().toString());
+                int height = Integer.parseInt(txtHeight.getText().toString());
+                try {
+                    compressedImage = new Compressor(MainActivity.this)
+                            .setMaxWidth(width).setMaxHeight(height)
+                            .setQuality(quality).setCompressFormat(Bitmap.CompressFormat.JPEG)
+                            .setDestinationDirectoryPath(filepath).compressToFile(originalImage);
+                    File finalFile = new File(filepath,originalImage.getName()) ;
+                    Bitmap finalBitmap = BitmapFactory.decodeFile(finalFile.getAbsolutePath()) ;
+                    imgCompressed.setImageBitmap(finalBitmap);
+                    txtCompressed.setText("Size : " + Formatter.formatShortFileSize(MainActivity.this, finalFile.length()));
+                    Toast.makeText(MainActivity.this,"Image Compressed and saved",Toast.LENGTH_SHORT).show() ;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"Error while compressing",Toast.LENGTH_SHORT).show() ;
+                }
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, RESULT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            btnCompress.setVisibility(View.VISIBLE);
+            final Uri imageUri = data.getData();
+            try {
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imgOriginal.setImageBitmap(selectedImage);
+                originalImage = new File(imageUri.getPath().replace("/raw", ""));
+                txtOriginal.setText("Size " + Formatter.formatShortFileSize(this, originalImage.length()));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+            }
+        } else {
+            Toast.makeText(this, "No Image Selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void askPermission() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
